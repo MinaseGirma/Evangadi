55f2ddb789ef7bbcfa9211307771948d7310c86d
Minase Girma <109416852+MinaseGirma@users.noreply.github.com>
2025-10-30 20:10:10 +0100

    axios

--- Server/app.js
+++ Server/app.js
@@ -13,7 +13,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://evangadiforummg.netlify.app"
+      // "https://evangadiforummg.netlify.app"
     ],
     credentials: true,
   })
